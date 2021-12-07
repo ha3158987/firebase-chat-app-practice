17bc7443ba5855b8b0170f3dabdddc44b217b727
Hyuna Park <hyuna_park@adenasoft.com>
2021-12-07 09:47:30 +0900

    feat: submit시 에러메세지 보여주는 기능 구현

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,6 +1,7 @@
-import React, { useState, useRef } from 'react'
-import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import React, { useState, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const RegisterPage = () => {
     const { register, watch, formState: { errors }, handleSubmit} = useForm();
@@ -11,9 +12,20 @@ const RegisterPage = () => {
     password.current = watch("password"); // *password 인풋에 들어오는 값을 담음.
 
     const onSubmitForm = async (data) => {
-        try {
+        //* firebase에 이메일과 비번을 만들어주기
+        //* data로 react-hook-form의 input에 들어온 정보들이 들어온다.
 
-        } catch (err) {}
+        try {
+            const auth = getAuth();
+            let createdUser = await createUserWithEmailAndPassword(auth, data.email, data.password)
+
+        } catch (err) {
+            console.log("err", err);
+            setErrorFromSubmit(err.message)
+            setTimeout(() => {
+                setErrorFromSubmit("")
+            }, 5000)
+        }
     }
 
     return (
@@ -37,7 +49,7 @@ const RegisterPage = () => {
                 <input name="password_confirm" type="password" {...register("password_confirm", {required: true, validate: (value) => value === password.current})}/>
                 {errors.password_confirm && errors.password_confirm.type === "required" && <p>위 항목은 필수정보입니다.</p>}
                 {errors.password_confirm && errors.password_confirm.type === "validate" && <p>비밀번호와 일치하지 않습니다.</p>}
-
+                {/* error가 있다면 여기서 보여주기 */}
                 {errorFromSubmit && <p>(errorFromSubmit)</p>}
                 <input type="submit" disabled={loading}/>
                 <Link style={{color: "gray", textDecoration: 'none'}} to="/login">이미 아이디가 있으신가요?</Link>
